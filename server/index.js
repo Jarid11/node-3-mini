9c6940aae8b8ae1b889275ad2d3731860de1fad3
Jarid11 <Marinos1995@gmail.com>
2018-04-03 18:55:06 -0500

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -1,16 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+const mc = require(`./controllers/messages_controller`);
+
+const createInitialSession = require(`./middlewares/session`);
+const filter = require(`./middlewares/filter`);
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 10000
+    }
+  })
+);
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+  const { method } = req;
+
+  if (method === "POST" || method === "PUT") {
+    filter(req, res, next);
+  } else {
+    next();
+  }
+});
+
+app.post("/api/messages", mc.create);
+app.get("/api/messages", mc.read);
+app.put("/api/messages", mc.update);
+app.delete("/api/messages", mc.delete);
+app.get("/api/messages/history", mc.history);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
